5b1e8603eb05646f8e54915102512454a228498b
Max Lee <maxolee23@gmail.com>
2019-06-20 18:57:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(musicians, instruments) {
   const array = []
 
   for (let i = 0, l = musicians.length; i < l; i++) {
-    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
   }
 
   return array;
